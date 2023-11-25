57c89508696f68a21caa6f4eefa9c75d6f17b710
frapercan <frapercan1@alum.us.es>
2023-11-25 10:43:52 +0100

    * lint fix * SQL doc

--- protein_data_handler/models/uniprot.py
+++ protein_data_handler/models/uniprot.py
@@ -5,8 +5,35 @@ Base = declarative_base()
 
 
 class Proteina(Base):
+    """
+        Representa una proteína con sus propiedades y relaciones.
+
+        Attributes:
+            entry_name (str): Nombre de entrada único para la proteína, sirve como clave primaria.
+            data_class (str): Clase de datos de la proteína.
+            molecule_type (str): Tipo de molécula de la proteína.
+            sequence_length (int): Longitud de la secuencia de la proteína.
+            accessions (relationship): Relación con la entidad 'Accession'.
+            created_date (Date): Fecha de creación del registro de la proteína.
+            sequence_update_date (Date): Fecha de la última actualización de la secuencia.
+            annotation_update_date (Date): Fecha de la última actualización de la anotación.
+            description (str): Descripción de la proteína.
+            gene_name (str): Nombre del gen asociado a la proteína.
+            organism (str): Organismo del que proviene la proteína.
+            organelle (str): Orgánulo asociado a la proteína.
+            organism_classification (str): Clasificación del organismo.
+            taxonomy_id (str): ID de taxonomía del organismo.
+            host_organism (str): Organismo huésped de la proteína.
+            host_taxonomy_id (str): ID de taxonomía del organismo huésped.
+            comments (str): Comentarios adicionales sobre la proteína.
+            pdb_references (relationship): Relación con la entidad 'PDBReference'.
+            go_terms (relationship): Relación con la entidad 'GOTerm'.
+            keywords (str): Palabras clave asociadas a la proteína.
+            protein_existence (int): Indicador de la existencia de la proteína.
+            seqinfo (str): Información adicional sobre la secuencia.
+        """
     __tablename__ = "proteinas"
-    entry_name = Column(String,primary_key=True, unique=True, nullable=False)
+    entry_name = Column(String, primary_key=True, unique=True, nullable=False)
     data_class = Column(String)
     molecule_type = Column(String)
     sequence_length = Column(Integer)
@@ -35,6 +62,15 @@ class Proteina(Base):
 
 
 class Accession(Base):
+    """
+        Representa un código de acceso para una proteína.
+
+        Attributes:
+            id (int): Identificador único para el acceso.
+            accession_code (str): Código de acceso único para la proteína.
+            proteina_entry_name (str): Nombre de entrada de la proteína asociada.
+            proteina (relationship): Relación con la entidad 'Proteina'.
+        """
     __tablename__ = "accessions"
     id = Column(Integer, primary_key=True)
     accession_code = Column(String, unique=True, nullable=False)
@@ -43,6 +79,18 @@ class Accession(Base):
 
 
 class PDBReference(Base):
+    """
+        Representa una referencia a la base de datos de estructuras de proteínas (PDB).
+
+        Attributes:
+            id (int): Identificador único para la referencia PDB.
+            pdb_id (str): Identificador único en la base de datos PDB.
+            proteina_entry_name (str): Nombre de entrada de la proteína asociada.
+            proteina (relationship): Relación con la entidad 'Proteina'.
+            method (str): Método utilizado para la determinación de la estructura.
+            resolution (str): Resolución de la estructura.
+            chains (str): Cadenas involucradas en la estructura.
+        """
     __tablename__ = "pdb_references"
     id = Column(Integer, primary_key=True)
     pdb_id = Column(String, nullable=False)
@@ -56,6 +104,17 @@ class PDBReference(Base):
 
 
 class GOTerm(Base):
+    """
+        Representa un término del Gene Ontology asociado a una proteína.
+
+        Attributes:
+            id (int): Identificador único para el término GO.
+            go_id (str): Identificador único en Gene Ontology.
+            proteina_entry_name (str): Nombre de entrada de la proteína asociada.
+            proteina (relationship): Relación con la entidad 'Proteina'.
+            category (str): Categoría del término GO.
+            description (str): Descripción del término GO.
+        """
     __tablename__ = "go_terms"
     id = Column(Integer, primary_key=True)
     go_id = Column(String, nullable=False)
