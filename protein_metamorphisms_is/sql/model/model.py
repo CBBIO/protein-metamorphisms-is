389f22ce0e06bd7371e030e695bea0b28540bd4a
xaxi <frapercan1@alum.us.es>
2025-01-05 11:13:07 +0100

    add module to import missing ORM schemas

---
+++ protein_metamorphisms_is/sql/model/model.py
@@ -0,0 +1,18 @@
+from protein_metamorphisms_is.operation.embedding.sequence_embedding import SequenceEmbeddingManager  # noqa: F401
+from protein_metamorphisms_is.operation.extraction.accessions import AccessionManager  # noqa: F401
+from protein_metamorphisms_is.operation.extraction.uniprot import UniProtExtractor  # noqa: F401
+
+from protein_metamorphisms_is.pipelines.fantasia.embedder import SequenceEmbedder  # noqa: F401
+from protein_metamorphisms_is.pipelines.fantasia.lookup import EmbeddingLookUp  # noqa: F401
+
+from protein_metamorphisms_is.operation.extraction.pdb import PDBExtractor  # noqa: F401
+from protein_metamorphisms_is.operation.clustering.sequence_clustering import SequenceClustering  # noqa: F401
+from protein_metamorphisms_is.operation.clustering.structural_subclustering import \
+    StructuralSubClustering  # noqa: F401
+from protein_metamorphisms_is.operation.functional.annotation_transfer.sequence_go_annotation import \
+    SequenceGOAnnotation  # noqa: F401
+from protein_metamorphisms_is.operation.functional.multifunctionality.go_multifunctionality_metrics import \
+    GoMultifunctionalityMetrics  # noqa: F401
+from protein_metamorphisms_is.operation.structural_alignment.structural_alignment import \
+    StructuralAlignmentManager  # noqa: F401
+from protein_metamorphisms_is.operation.embedding.structure_3di import Structure3DiManager  # noqa: F401
