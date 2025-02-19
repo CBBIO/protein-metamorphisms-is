a4bbfd4a199c18e73a26dfaf1a9bdbb920e87e9a
FranciscoJRuiMot <pacopeperuiz.mota@gmail.com>
2025-02-19 10:55:30 +0100

    Fix? poetry

--- protein_metamorphisms_is/main.py
+++ protein_metamorphisms_is/main.py
@@ -22,11 +22,10 @@ def main(config_path='config/config.yaml'):
     UniProtExtractor(conf).start()
     PDBExtractor(conf).start()
     Structure3DiManager(conf).start()
-    Structure3DiManager(conf).start()
     SequenceClustering(conf).start()
     StructuralSubClustering(conf).start()
     StructuralAlignmentManager(conf).start()
-    SequenceEmbeddingManager(conf).start()
+    # SequenceEmbeddingManager(conf).start()
     SequenceGOAnnotation(conf).start()
     GoMultifunctionalityMetrics(conf).start()
 
