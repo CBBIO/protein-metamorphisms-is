913264f3247e9203762ec71a149cf440e1178bc0
FranciscoJRuiMot <pacopeperuiz.mota@gmail.com>
2025-03-31 11:36:00 +0200

    Better logs management in separated files

--- protein_metamorphisms_is/helpers/logger/logger.py
+++ protein_metamorphisms_is/helpers/logger/logger.py
@@ -2,30 +2,35 @@ import logging
 import os
 
 
-def setup_logger(name, log_path):
+def setup_logger(name, log_dir):
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-
-    log_dir = os.path.dirname(log_path)
-    if log_dir and not os.path.exists(log_dir):
-        os.makedirs(log_dir)
+    logger.setLevel(logging.DEBUG)
 
     logger.propagate = False
 
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.INFO)
+    if log_dir and not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    if log_dir:
+        info_path = os.path.join(log_dir, 'info.log')
+        info_handler = logging.FileHandler(info_path)
+        info_handler.setLevel(logging.INFO)
+        info_handler.setFormatter(formatter)
+        logger.addHandler(info_handler)
+
+        debug_path = os.path.join(log_dir, 'debug.log')
+        debug_handler = logging.FileHandler(debug_path)
+        debug_handler.setLevel(logging.DEBUG)
+        debug_handler.setFormatter(formatter)
+        logger.addHandler(debug_handler)
+
+        logger.info(f"Logs are being save to: {log_dir}")
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
-
-    if log_path:
-        file_handler = logging.FileHandler(log_path)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(formatter)
-
-        logger.addHandler(file_handler)
-        logger.info(f"Logs are being save to: {log_path}")
 
     return logger
