67114aad3734459b23e52735ae5d2fa4f8b8eab2
frapercan <frapercan1@alum.us.es>
2025-06-01 22:01:04 +0200

    restructured

--- protein_metamorphisms_is/main.py
+++ protein_metamorphisms_is/main.py
@@ -1,11 +1,14 @@
 import logging
 
+from protein_metamorphisms_is.helpers.services.services import check_services
+from protein_metamorphisms_is.operation.embedding.structure_3di import Structure3DiManager
+
+
 
 def main(config_path='config/config.yaml'):
     from protein_metamorphisms_is.helpers.config.yaml import read_yaml_config
     conf = read_yaml_config(config_path)
 
-    # ✅ Mover esto al principio
     logger = logging.getLogger("protein_metamorphisms_is")
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
@@ -13,21 +16,27 @@ def main(config_path='config/config.yaml'):
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    # Step 1: Ensure pgvector is available before ORM definitions
-    from protein_metamorphisms_is.sql.base.database_manager import DatabaseManager
-    DatabaseManager(conf)
 
-    # Step 2: Import ORM-based logic after pgvector is ready
+    # Step 1: Import ORM-based logic & check model coherence
     from protein_metamorphisms_is.sql.model.model import (
         AccessionManager,
         UniProtExtractor,
-        SequenceEmbeddingManager
+        PDBExtractor,
+        SequenceEmbeddingManager,
+        Structure3DiManager
     )
+
+    # Step 2: Check services running
+
+    check_services(conf, logger)
+
 
     # Step 3: Run components
     AccessionManager(conf).load_accessions_from_csv()
     UniProtExtractor(conf).start()
+    PDBExtractor(conf).start()
     SequenceEmbeddingManager(conf).start()
+    Structure3DiManager(conf).start()
 
 
 if __name__ == '__main__':
