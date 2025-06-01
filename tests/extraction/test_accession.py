766b14caa29fde1d7afc23efe77cc191bb40c1f4
frapercan <frapercan1@alum.us.es>
2025-06-01 22:14:20 +0200

    test fix

--- tests/extraction/test_accession.py
+++ tests/extraction/test_accession.py
@@ -6,17 +6,6 @@ import pytest
 
 from protein_metamorphisms_is.helpers.config.yaml import read_yaml_config
 from protein_metamorphisms_is.operation.extraction.accessions import AccessionManager
-from protein_metamorphisms_is.operation.extraction.pdb import PDBExtractor
-from protein_metamorphisms_is.operation.extraction.uniprot import UniProtExtractor
-from protein_metamorphisms_is.operation.embedding.sequence_embedding import SequenceEmbeddingManager
-from protein_metamorphisms_is.operation.embedding.structure_3di import Structure3DiManager
-from protein_metamorphisms_is.operation.clustering.sequence_clustering import SequenceClustering
-from protein_metamorphisms_is.operation.clustering.structural_subclustering import StructuralSubClustering
-from protein_metamorphisms_is.operation.functional.annotation_transfer.sequence_go_annotation import \
-    SequenceGOAnnotation
-from protein_metamorphisms_is.operation.structural_alignment.structural_alignment import StructuralAlignmentManager
-from protein_metamorphisms_is.operation.functional.multifunctionality.go_multifunctionality_metrics import \
-    GoMultifunctionalityMetrics
 from protein_metamorphisms_is.sql.model.entities.protein.accesion import Accession
 
 @pytest.mark.order(1)
